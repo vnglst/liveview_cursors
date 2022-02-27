0cc3b3578d5366378be369e7e53e46e1a8ceb9c8
Koen van Gilst <koen@koenvangilst.nl>
2022-02-27 22:21:14 +0100

    use session to identify users in the cursor view

--- lib/liveview_cursors_web/controllers/page_controller.ex
+++ lib/liveview_cursors_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule LiveviewCursorsWeb.PageController do
   use LiveviewCursorsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    session = conn |> get_session()
+
+    case session do
+      %{"user" => _user} ->
+        conn
+        |> redirect(to: "/cursors")
+
+      _ ->
+        conn
+        |> put_session(:user, LiveviewCursorsWeb.Name.generate())
+        |> configure_session(renew: true)
+        |> redirect(to: "/cursors")
+    end
   end
 end
--- lib/liveview_cursors_web/live/cursors.ex
+++ lib/liveview_cursors_web/live/cursors.ex
@@ -1,13 +1,19 @@
 defmodule LiveviewCursorsWeb.Cursors do
   use LiveviewCursorsWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  def mount(_params, %{"user" => user}, socket) do
     updated =
       socket
       |> assign(:x, 50)
       |> assign(:y, 50)
+      |> assign(:user, user)
 
     {:ok, updated}
+  end
+
+  # if no user name, redirect to root to generate one
+  def mount(_params, _session, socket) do
+    {:ok, socket |> redirect(to: "/")}
   end
 
   def handle_event("cursor-move", %{"x" => x, "y" => y}, socket) do
@@ -22,7 +28,7 @@ defmodule LiveviewCursorsWeb.Cursors do
   def render(assigns) do
     ~H"""
     <ul class="list-none" id="cursors" phx-hook="TrackClientCursor">
-      <li style={"color: deeppink; left: #{@x}%; top: #{@y}%"} class="absolute">
+      <li style={"color: deeppink; left: #{@x}%; top: #{@y}%"} class="flex flex-col absolute pointer-events-none whitespace-nowrap overflow-hidden">
         <svg
         version="1.1"
         width="25px"
@@ -38,6 +44,7 @@ defmodule LiveviewCursorsWeb.Cursors do
           points="9.2,7.3 9.2,18.5 12.2,15.6 12.6,15.5 17.4,15.5"
           />
         </svg>
+        <span style={"background-color: deeppink;"} class="mt-1 ml-4 px-1 text-sm text-white"><%= @user %></span>
       </li>
     </ul>
     """
